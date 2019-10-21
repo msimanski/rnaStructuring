d3e1b8b51ed3b6981e0e085d79e3d5f0c13545ac
Michael Simanski <mfsimanski@gmail.com>
2019-10-21 12:10:02 -0400

    Initial commit

---
+++ src/rnaStructuring/Base.java
@@ -0,0 +1,6 @@
+package rnaStructuring;
+
+public class Base
+{
+
+}
---
+++ src/rnaStructuring/Strand.java
@@ -0,0 +1,12 @@
+package rnaStructuring;
+
+public class Strand
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
